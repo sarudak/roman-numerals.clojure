fd791c22475d569edc0d222d4c413b6b7d05fd54
Sarudak <SPaulson@paraport.com>
2013-05-29 13:25:53 -0700

    Added converter class with base methods and test class

---
+++ src/roman_numeral/converter.clj
@@ -0,0 +1,5 @@
+(ns roman-numeral.converter)
+
+(defn parse-roman-numeral [roman-numeral] 0)
+
+(defn convert-to-roman-numeral [number] "")
--- test/roman_numeral/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns roman-numeral.core-test
-  (:use clojure.test
-        roman-numeral.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
