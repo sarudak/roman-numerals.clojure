bb540330f94ae85900ffec27aa6edc69bccd400b
Sarudak <SPaulson@paraport.com>
2013-05-29 13:21:44 -0700

    Added dependencies and intelliJ project

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject roman-numeral "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Transforms numbers into roman numerals and back again"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+				 [midje "1.5.1"]]
+  :plugins [[lein-idea "1.0.1"]
+			[lein-midje "3.0.0"]]
+	  )
+	
