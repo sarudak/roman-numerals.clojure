8566449d91471c0bc4015b055b09d370430fa223
Sarudak <SPaulson@paraport.com>
2013-05-30 15:12:31 -0700

    Added fist pass at group implementation

--- src/roman_numeral/converter.clj
+++ src/roman_numeral/converter.clj
@@ -1,5 +1,34 @@
 (ns roman-numeral.converter)
 
-(defn parse-roman-numeral [roman-numeral] 0)
+(defn get-value-of-pair [[left right]]
+  (if (< left right) (* -1 left) left)
+ )
+
+(defn get-pairs [numerals]
+ (partition 2 1 (flatten [numerals 0]))
+  )
+
+(defn get-value-of-pairs [pairs]
+  (map get-value-of-pair pairs)
+  )
+
+(defn parse-roman-numeral [roman-numeral]
+  (let [character-map {
+                         \I 1
+                          \V 5
+                          \X 10
+                          \L 50
+                          \C 100
+                          \D 500
+                          \M 1000}]
+
+    (->>
+      (map character-map roman-numeral)
+      (get-pairs)
+      (get-value-of-pairs)
+      (apply +))
+  ))
+
+
 
 (defn convert-to-roman-numeral [number] "")
--- src/test/converter.clj
+++ src/test/converter.clj
@@ -4,27 +4,27 @@
 
 (fact "When parsing roman numerals symbols are valued correctly"
   (parse-roman-numeral "I") => 1
-;  (parse-roman-numeral "V") => 5
-;  (parse-roman-numeral "X") => 10
-;  (parse-roman-numeral "L") => 50
-;  (parse-roman-numeral "C") => 100
-;  (parse-roman-numeral "D") => 500
-;  (parse-roman-numeral "M") => 1000
+  (parse-roman-numeral "V") => 5
+  (parse-roman-numeral "X") => 10
+  (parse-roman-numeral "L") => 50
+  (parse-roman-numeral "C") => 100
+  (parse-roman-numeral "D") => 500
+  (parse-roman-numeral "M") => 1000
   )
 
-;(fact "When parsing roman numerals the value of symbols are added together"
-;  (parse-roman-numeral "III") => 3
-;  (parse-roman-numeral "XV") => 15
-;  (parse-roman-numeral "MDCC") => 1700
-;  (parse-roman-numeral "MMMCCCXXXIII") => 3333
-;  )
+(fact "When parsing roman numerals the value of symbols are added together"
+  (parse-roman-numeral "III") => 3
+  (parse-roman-numeral "XV") => 15
+  (parse-roman-numeral "MDCC") => 1700
+  (parse-roman-numeral "MMMCCCXXXIII") => 3333
+  )
 ;
-;(fact "When a lower value symbol occurs before a higher value symbol it's value is subtracted from the higher value symbol"
-;  (parse-roman-numeral "IV") => 4
-;  (parse-roman-numeral "CD") => 400
-;  (parse-roman-numeral "IX") => 9
-;  (parse-roman-numeral "CMXCIX") => 999
-;  )
+(fact "When a lower value symbol occurs before a higher value symbol it's value is subtracted from the higher value symbol"
+  (parse-roman-numeral "IV") => 4
+  (parse-roman-numeral "CD") => 400
+  (parse-roman-numeral "IX") => 9
+  (parse-roman-numeral "CMXCIX") => 999
+  )
 
 ;(fact "When converting a number to a roman numeral a correct roman numeral representation is returned"
 ;  (convert-to-roman-numeral 1) => "I"
