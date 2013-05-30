0cdce35bfd83a95b07b1af20f33dab5fddf36af5
Sarudak <SPaulson@paraport.com>
2013-05-30 11:35:49 -0700

    Added tests for converting to roman numerals.

--- src/test/converter.clj
+++ src/test/converter.clj
@@ -25,3 +25,19 @@
 ;  (parse-roman-numeral "IX") => 9
 ;  (parse-roman-numeral "CMXCIX") => 999
 ;  )
+
+;(fact "When converting a number to a roman numeral a correct roman numeral representation is returned"
+;  (convert-to-roman-numeral 1) => "I"
+;  (convert-to-roman-numeral 3) => "III"
+;  (convert-to-roman-numeral 4) => "IV"
+;  (convert-to-roman-numeral 5) => "V"
+;  (convert-to-roman-numeral 9) => "IX"
+;  (convert-to-roman-numeral 10) => "X"
+;  (convert-to-roman-numeral 15) => "XV"
+;  (convert-to-roman-numeral 50) => "L"
+;  (convert-to-roman-numeral 54) => "LIV"
+;  (convert-to-roman-numeral 100) => "C"
+;  (convert-to-roman-numeral 222) => "CCXXII"
+;  (convert-to-roman-numeral 555) => "DLV"
+;  (convert-to-roman-numeral 1030) => "MXXX"
+;  )
